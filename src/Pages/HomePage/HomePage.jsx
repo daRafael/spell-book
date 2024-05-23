e3de4c1d343d76849c222af5c9e4544add954275
Rafael Guerra <rafa99gm@hotmail.com>
2024-05-23 22:05:10 +0100

    home-page-css

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -5,9 +5,16 @@ import './HomePage.css'
 export default function HomePage() {
   return (
     <div className='homepage'>
-      <Link to='/add-spellbook'>
-        <button>Create Your Spellbook</button>
-      </Link>
+      <div className='hero-page-title-btn-container'>
+        <div className='hero-page-title'>
+          Create a Spellbook for all your Dungeons & Dragons characters!
+        </div>
+        <div className='hero-page-button-container'>
+          <Link to='/add-spellbook'>
+          <button className='hero-page-btn'>Click here!</button>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
